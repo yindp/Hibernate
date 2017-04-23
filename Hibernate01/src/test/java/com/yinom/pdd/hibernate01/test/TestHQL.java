36fb91ee472ed2eb360d04cf7b2370a1afb69e85
yindp <378714297@qq.com>
2017-04-23 20:14:44 +0800

    HQL query depart properties.

--- Hibernate01/src/test/java/com/yinom/pdd/hibernate01/test/TestHQL.java
+++ Hibernate01/src/test/java/com/yinom/pdd/hibernate01/test/TestHQL.java
@@ -13,20 +13,42 @@ import java.util.List;
  */
 public class TestHQL {
     @Test
-    public void test() {
+    public void testQueryAll() {
         Session session = null;
         try {
             session = MySessionFactory.openSession();
-            String hql = "from Employee";
             List<Employee> employeeList = session.createQuery("from Employee").list();
             for (Employee employee : employeeList) {
-                System.out.println(employee.getNickname()+":"+employee.getDepartment().getName());
+                System.out.println(employee.getNickname() + ":" + employee.getDepartment().getName());
             }
             System.out.println("*****************************");
             Iterator<Employee> employeeIterator = employeeList.iterator();
             while (employeeIterator.hasNext()) {
                 Employee employee = employeeIterator.next();
-                System.out.println(employee.getNickname()+":"+employee.getDepartment().getName());
+                System.out.println(employee.getNickname() + ":" + employee.getDepartment().getName());
+            }
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        } finally {
+            MySessionFactory.close(session);
+        }
+    }
+
+    @Test
+    public void tesDepart() {
+        Session session = null;
+        try {
+            session = MySessionFactory.openSession();
+            List<Object[]> objectList = session.createQuery("select username,salary from Employee").list();
+            for (Object[] obj : objectList) {
+                System.out.println(obj[0] + ":" + obj[1]);
+            }
+            System.out.println("*************************************");
+            Iterator<Object[]> it = objectList.iterator();
+            while (it.hasNext()) {
+                Object[] objects = it.next();
+                System.out.println(objects[0] + ":" + objects[1]);
             }
         } catch (Exception e) {
 
